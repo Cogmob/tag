c5e11b6992a83c4049d97c847e4942b63aa60583
cogmob <luke.avery@live.co.uk>
2016-05-26 14:54:32 +0100

    added test files for pyfakefs and pyfilesystem

---
+++ src/python/tests/test_pyfakefs.py
@@ -0,0 +1,7 @@
+from compare import expect
+from nose.plugins.attrib import attr
+import unittest
+
+class TestPyfaefs(unittest.TestCase):
+    def test_create_a_file(self):
+        return 1
---
+++ src/python/tests/test_pyfilesystem.py
@@ -0,0 +1,7 @@
+from compare import expect
+from nose.plugins.attrib import attr
+import unittest
+
+class TestPyfilesystem(unittest.TestCase):
+    def test_ls(self):
+        return 1
