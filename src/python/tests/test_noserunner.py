9590c5482ce14e7f6f976ce2cd26cb4c4418ab23
cogmob <luke.avery@live.co.uk>
2016-05-27 11:06:08 +0100

    changed tests to not be class based and made first fakeos test

--- src/python/tests/test_noserunner.py
+++ src/python/tests/test_noserunner.py
@@ -2,10 +2,8 @@ from compare import expect
 from nose.plugins.attrib import attr
 import unittest
 
-class TestNoserunner(unittest.TestCase):
-    @attr('example')
-    def test_if_this_test_can_be_selected(self):
-        expect(1).to_equal(1)
+def test_if_this_test_can_be_selected():
+    expect(1).to_equal(1)
 
-    def test_basic_assert(self):
-        assert True
+def test_basic_assert():
+    assert True
--- src/python/tests/test_pyfakefs.py
+++ src/python/tests/test_pyfakefs.py
@@ -1,7 +1,21 @@
 from compare import expect
+from with_setup_args import with_setup_args
 from nose.plugins.attrib import attr
 import unittest
 
-class TestPyfaefs(unittest.TestCase):
-    def test_create_a_file(self):
-        return 1
+import os
+import pyfakefs.fake_filesystem as fake_fs
+
+def setup():
+    fs = fake_fs.FakeFilesystem(path_separator='/')
+    os = fake_fs.FakeOsModule(fs)
+    return [fs, os], {}
+
+def tear_down(fs, os):
+    pass
+
+@with_setup_args(setup, tear_down)
+def test_create_a_file(fs, os):
+    fs.CreateFile('test-file.txt')
+    files = os.listdir('.')
+    expect(files).to_equal(['test-file.txt'])
