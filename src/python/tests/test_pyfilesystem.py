3138bc89eac65a4c672d28e1a35612adc9177a15
cogmob <luke.avery@live.co.uk>
2016-06-09 18:03:07 +0100

    created selected test script

--- src/python/tests/test_pyfilesystem.py
+++ src/python/tests/test_pyfilesystem.py
@@ -9,9 +9,24 @@ def up():
     app_fs = fsopendir('mount://src/python/tests/data/fs.ini', create_dir=True)
     app_fs.makedir('tmp/.tagger')
     app_fs.makedir('tmp/.filtered')
+    app_fs.makedir('tmp/example')
+    app_fs.makedir('tmp/example/subdir')
+    app_fs.createfile('tmp/example/file1')
+    app_fs.createfile('tmp/example/file2')
+    app_fs.createfile('tmp/example/subdir/file3')
     return [app_fs], {}
 
 @s(up)
 def test_ls(app_fs):
     files = app_fs.listdir('tmp')
-    expect(files).to_equal(['.tagger', '.filtered'])
+    expect(files).to_equal(['.tagger', 'example', '.filtered'])
+
+@s(up)
+def test_create_file(app_fs):
+    files = app_fs.listdir('tmp')
+    expect(files).to_equal(['.tagger', 'example', '.filtered'])
+
+@s(up)
+def test_create_several_files(app_fs):
+    files = app_fs.listdir('tmp')
+    expect(files).to_equal(['.tagger', 'example', '.filtered'])
