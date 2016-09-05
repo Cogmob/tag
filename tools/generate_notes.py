26058e6c57dc49294738fd4d054b44858f938a9c
cogmob <luke.avery@live.co.uk>
2016-09-05 10:34:29 +0100

    began the script to create the notes

--- tools/generate_notes.py
+++ tools/generate_notes.py
@@ -1,1 +1,17 @@
-print "working!"
+import glob
+import sys
+
+print(sys.version)
+
+print 'working!'
+
+with open('tools/generate_notes.py', 'r') as source:
+    print source.read()
+
+print ''
+print ''
+print ''
+print ''
+
+for filename in glob.iglog('src/notes/**', recursive=True):
+    print(filename)
