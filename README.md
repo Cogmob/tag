49a76ba0f290d2afeb3935ee105f48e0561d37c7
cogmob <luke.avery@live.co.uk>
2016-09-07 00:07:20 +0100

    script to compile notes finished

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # installation
 
-cd ~/tag  
-./tools/create_virtualenv
+    cd ~/downloads
+    wget https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py
+    pip install virtualenv
+
+    cd ~/tag  
+    ./tools/create_virtualenv
--- tools/generate_notes.py
+++ tools/generate_notes.py
@@ -1,19 +1,64 @@
 import os
 import fnmatch
+from subprocess import call
 
 def main():
+    lines = get_start()
+
     matches = []
-    for root, dirnames, filenames in os.walk('src'):
-        for filename in fnmatch.filter(filenames, '*.md'):
+    for root, dirnames, filenames in os.walk('.gen/src/notes'):
+        for filename in fnmatch.filter(filenames, '*'):
                 matches.append(os.path.join(root, filename))
+
     for filename in matches:
-        print(filename)
-        print(change_extension(filename, 'html'))
-        with open(filename) as f:
-            print(f.read())
+        call(['vim', '-c', 'set ft=txtfmt', '-c', 'TOhtml', '+wa', '+qa', filename])
+        with open(filename + '.html') as f:
+            lines += [get_title_line(filename)]
+            lines += format_titles(get_content(f.read()))
 
+    lines += ['</body>']
+    lines += ['</html>']
 
-def change_extension(filename, new_extension):
-    return filename
+    with open('.gen/src/notes/index.html', 'w+') as f:
+        f.write('\n'.join(lines))
+
+def change_extension(filename, old_extension, new_extension):
+    return '%s.%s' % (filename.split('.'+old_extension)[0], new_extension)
+
+def get_content(page):
+    lines = page.split('\n')
+    start = find_line_number(lines=lines, target='/style') + 1
+    end = find_line_number(lines=lines, target='/body', start=start)
+    return lines[start:end]
+
+def find_line_number(lines, target, start=0):
+    for i in range(start, len(lines)):
+        if target in lines[i]:
+            return i
+    raise ValueError('target string not found')
+
+def get_start():
+    with open('tools/start.html', 'r') as f:
+        return f.read().split('\n')
+    raise IOError('start.html file missing')
+
+def get_title_line(filename):
+    return '<span class="title0">' + filename.split('/')[-1] + '</span>'
+
+def format_titles(lines):
+    ret = []
+    for line in lines:
+        if '>###' in line:
+            newline = line.split('>###')
+            ret += ['<span class="title3">' + newline[1] + '</span>']
+        elif '>##' in line:
+            newline = line.split('>##')
+            ret += ['<span class="title2">' + newline[1] + '</span>']
+        elif '>#' in line:
+            newline = line.split('>#')
+            ret += ['<span class="title1">' + newline[1] + '</span>']
+        else:
+            ret += [line]
+    return ret
 
 main()
