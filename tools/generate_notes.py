cb29edfe94a05099492223e2a507568268222f4b
cogmob <luke.avery@live.co.uk>
2016-09-07 12:05:52 +0100

    allow folding of notes in webpage

--- tools/generate_notes.py
+++ tools/generate_notes.py
@@ -13,14 +13,23 @@ def main():
     for filename in matches:
         call(['vim', '-c', 'set ft=txtfmt', '-c', 'TOhtml', '+wa', '+qa', filename])
         with open(filename + '.html') as f:
+            lines += ['<details>']
             lines += [get_title_line(filename)]
-            lines += format_titles(get_content(f.read()))
+            lines += format_lines(get_content(f.read()))
+            lines += ['</details>']
 
     lines += ['</body>']
     lines += ['</html>']
 
     with open('.gen/src/notes/index.html', 'w+') as f:
         f.write('\n'.join(lines))
+
+def format_lines(lines):
+    ret = []
+    for line in lines:
+        newline = format_title(format_bullet(line))
+        ret += [newline]
+    return ret
 
 def change_extension(filename, old_extension, new_extension):
     return '%s.%s' % (filename.split('.'+old_extension)[0], new_extension)
@@ -43,22 +52,21 @@ def get_start():
     raise IOError('start.html file missing')
 
 def get_title_line(filename):
-    return '<span class="title0">' + filename.split('/')[-1] + '</span>'
+    return '<summary class="title0">' + filename.split('/')[-1] + '</summary>'
 
-def format_titles(lines):
-    ret = []
-    for line in lines:
-        if '>###' in line:
-            newline = line.split('>###')
-            ret += ['<span class="title3">' + newline[1] + '</span>']
-        elif '>##' in line:
-            newline = line.split('>##')
-            ret += ['<span class="title2">' + newline[1] + '</span>']
-        elif '>#' in line:
-            newline = line.split('>#')
-            ret += ['<span class="title1">' + newline[1] + '</span>']
-        else:
-            ret += [line]
-    return ret
+def format_title(line):
+    if '>###' in line:
+        newline = line.split('>###')
+        return '<span class="title3">' + newline[1] + '</span>'
+    if '>##' in line:
+        newline = line.split('>##')
+        return '<span class="title2">' + newline[1] + '</span>'
+    if '>#' in line:
+        newline = line.split('>#')
+        return '<span class="title1">' + newline[1] + '</span>'
+    return line
+
+def format_bullet(line):
+    return line.replace('>. ', '>• ').replace(' . ', ' • ')
 
 main()
