82e21075b3c2f724b412d47d8ee83ad6cdff23bc
cogmob <luke.avery@live.co.uk>
2016-09-23 16:38:45 +0100

    further work on second example

--- src/python/create_view/create_view.py
+++ src/python/create_view/create_view.py
@@ -1,7 +1,13 @@
-from simplads import Bundle, pipe, lift, add_data, add_first_data
+from simplads import Bundle, pipe, lift, add_data, add_first_data, pr
 from .steps.create_root import create_root
 from .steps.add_files import add_files
 from .steps.simplify import simplify
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+def pri(i):
+    print(pprint.pprint(i, indent=4))
+    return i
 
 def create_view(view_files, example_files):
     return pipe(
@@ -9,5 +15,6 @@ def create_view(view_files, example_files):
         create_root,
         add_first_data('directory',
             'files', example_files),
+        pri,
         add_files,
         simplify)
