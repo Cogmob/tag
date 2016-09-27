0c669c2e884322349c114edd10c330fa497c33cc
cogmob <luke.avery@live.co.uk>
2016-09-27 17:14:05 +0100

    added epr

--- src/python/create_view/create_view.py
+++ src/python/create_view/create_view.py
@@ -3,6 +3,7 @@ from .steps.create_root import create_root
 from .steps.add_files import add_files
 from .steps.simplify import simplify
 from .steps.add_view_files import add_view_files
+from epr import epr
 
 def create_view(view_files, example_files):
     return pipe(
@@ -12,5 +13,6 @@ def create_view(view_files, example_files):
         add_data('files', example_files),
         remove_data('view_files'),
         add_files,
+        epr,
         remove_data('files'),
         simplify)
--- src/python/create_view/steps/add_view_files.py
+++ src/python/create_view/steps/add_view_files.py
@@ -1,6 +1,4 @@
 from termcolor import colored
 
 def add_view_files(i):
-    print(colored('hello', 'red'), colored('world', 'green'))
-    print(i)
     return i
