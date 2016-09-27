f037f1525aa688526bb69d7510f80cc5f70f2c3c
cogmob <luke.avery@live.co.uk>
2016-09-27 09:58:30 +0100

    test push

---
+++ src/python/create_view/steps/add_view_files.py
@@ -0,0 +1,6 @@
+from termcolor import colored
+
+def add_view_files(i):
+    print(colored('hello', 'red'), colored('world', 'green'))
+    print(i)
+    return i
--- src/python/create_view/steps/simplify.py
+++ src/python/create_view/steps/simplify.py
@@ -12,8 +12,7 @@ def simplify(i, path=[]):
     files = []
     if 'stored_files' in i:
         for stored_file in i['stored_files']:
-            tags =
-                [str(i) for i in stored_file['primary_tags'] if i not in path]
+            tags = [str(i) for i in stored_file['primary_tags'] if i not in path]
             files.append('__'.join([i for i in tags if i not in path]))
         if len(files) > 0:
             ret['files'] = sorted(files)
